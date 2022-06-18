88a192107b669eaef46ae4763d9393c6825438af
alamgirjuab <alamgirjuab@gmail.com>
2022-06-19 00:38:55 +0600

    link updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
@@ -44,6 +45,25 @@ async function run() {
             console.log(result);
             res.json(result);
         });
+
+        // DELETE API
+        /* app.delete('/formtotable/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('hit the delete api', id);
+            const query = { _id: ObjectId(id) };
+            const result = await dataFormToTable.deleteOne(query);
+            res.json(result);
+        }); */
+
+        app.delete('/formtotable/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await dataFormToTable.deleteOne(query);
+            res.json(result);
+
+        })
+
+
     } finally {
     }
 }
